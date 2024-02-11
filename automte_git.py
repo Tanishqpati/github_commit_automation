459fefcad4653e1470dab2924586c7849c77dbb4
Tanishq Patidar <tanishq.patidar@kalvium.community>
2024-02-11 18:37:52 +0530

    Added function main

--- automte_git.py
+++ automte_git.py
@@ -8,3 +8,9 @@ def modify_file(file_path, text_to_add):
 def git_add_commit(date, commit_message):
     subprocess.run(['git', 'add', '.'])
     subprocess.run(['git', 'commit', '--date=' + date, '-m', commit_message])
+
+def main():
+    
+
+if __name__ == "__main__":
+    main()
